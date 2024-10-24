94973258db3c32bf8b0ae64ade80c76c8ab174aa
unknown <rf.ch@interia.pl>
2024-10-24 22:36:22 +0200

    Dodano liste1

---
+++ FA/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;  
+
+class FA
+{
+    public static int NO_OF_CHARS = 256;
+
+    public static int getNextState(char[] pat, int M, int state, int x)
+    {
+        if (state < M && (char)x == pat[state])
+        {
+            return state + 1;
+        }
+
+        int ns, i;
+        for (ns = state; ns > 0; ns--)
+        {
+            if (pat[ns - 1] == (char)x)
+            {
+                for (i = 0; i < ns - 1; i++)
+                {
+                    if (pat[i] != pat[state - ns + 1 + i])
+                    {
+                        break;
+                    }
+                }
+                if (i == ns - 1)
+                {
+                    return ns;
+                }
+            }
+        }
+ababab abab
+        return 0;
+    }
+
+    public static void computeTF(char[] pat, int M, int[][] TF)
+    {
+        int state, x;
+        for (state = 0; state <= M; ++state)
+        {
+            for (x = 0; x < NO_OF_CHARS; ++x)
+            {
+                TF[state][x] = getNextState(pat, M, state, x);
+            }
+        }
+    }
+
+    public static void search(char[] pat, char[] txt)
+    {
+        int M = pat.Length;
+        int N = txt.Length;
+
+        int[][] TF = RectangularArrays.ReturnRectangularIntArray(M + 1, NO_OF_CHARS);
+
+        computeTF(pat, M, TF);
+        bool patternFound = false;
+        int i, state = 0;
+        for (i = 0; i < N; i++)
+        {
+            state = TF[state][txt[i]];
+            if (state == M)
+            {
+                Console.WriteLine("Pattern found at index " + (i - M + 1));
+                patternFound = true;
+            }
+            
+            
+        }
+
+        if (patternFound == false)
+        {
+            Console.WriteLine("Pattern Not Found");
+        }
+    }
+
+    public static class RectangularArrays
+    {
+        public static int[][] ReturnRectangularIntArray(int size1, int size2)
+        {
+            int[][] newArray = new int[size1][];
+            for (int array1 = 0; array1 < size1; array1++)
+            {
+                newArray[array1] = new int[size2];
+            }
+
+            return newArray;
+        }
+    }
+
+    
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Usage: <pattern> <filename>");
+            return;
+        }
+
+        string pattern = args[0];          
+        string filename = args[1];        
+
+        try
+        {
+           
+            string text = File.ReadAllText(filename);
+
+   
+            search(pattern.ToCharArray(), text.ToCharArray());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+}
---
+++ KMP/Program.cs
@@ -0,0 +1,100 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+class KMP {
+   
+    void computeLPSArray(string pat, int M, int[] lps) {
+
+        int len = 0;
+
+        lps[0] = 0;
+
+        int i = 1;
+        while (i < M) {
+            if (pat[i] == pat[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+    List<int> KMPSearch(string pat, string txt) {
+        int M = pat.Length;
+        int N = txt.Length;
+
+       
+        int[] lps = new int[M];
+        List<int> result = new List<int>();
+
+      
+        computeLPSArray(pat, M, lps);
+
+        int i = 0; 
+        int j = 0; 
+        while ((N - i) >= (M - j)) {
+            if (pat[j] == txt[i]) {
+                j++;
+                i++;
+            }
+
+            if (j == M) {
+                result.Add(i - j );
+                j = lps[j - 1];
+            } else if (i < N && pat[j] != txt[i]) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i = i + 1;
+                }
+            }
+        }
+
+       
+        return result;
+    }
+
+    static void printIndexes(List<int> results)
+    {
+        if (results.Count == 0)
+        {
+            Console.WriteLine("Pattern Not Found");
+        }
+        else
+        {
+            Console.WriteLine("Pattern found at index " + string.Join(", ", results));
+        }
+    }
+    
+   
+    static void Main(string [] args)
+    {
+        
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Valid number of arguments! Usage: <pattern> <filename>");
+            return;
+        }
+        string pattern = args[0];
+        string text = args[1];
+
+        string txt = File.ReadAllText(text);
+       
+        KMP kmp = new KMP();
+        List<int> result = kmp.KMPSearch(pattern, txt);
+        printIndexes(result);
+
+       
+        
+    }
+}
